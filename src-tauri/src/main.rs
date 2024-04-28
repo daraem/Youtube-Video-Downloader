1ab79e2287f089216c828d1f4c2425bf696f3a7c
daraem <daraemxd@gmail.com>
2024-04-28 20:56:41 +0100

    update

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,23 +4,24 @@
 use rusty_ytdl::{Video, VideoSearchOptions, VideoQuality, VideoOptions};
 use std::env;
 use std::fs;
+use std::path;
+use opener;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-async fn download(url: String, formatState: bool) {
+async fn download(url: String, format_state: bool) {
   let mut video_options = VideoOptions {
     quality: VideoQuality::Highest,
     filter: VideoSearchOptions::VideoAudio ,
     ..Default::default()
   };
 
-  let mut formatString = ".mp4";
+  let mut format_string = ".mp4";
 
-  if formatState {
+  if format_state {
     video_options.filter = VideoSearchOptions::Audio;
-    formatString = ".mp3";
+    format_string = ".mp3";
   }
-
 
   let video = Video::new_with_options(url, video_options).unwrap();
 
@@ -28,15 +29,25 @@ async fn download(url: String, formatState: bool) {
 
   let video_folder = "videos/";
   let videostr = (video_info.video_details.title).to_string();
-  let video_title = format!("{}{}{}", &video_folder, &videostr, &formatString);
+  let video_title = format!("{}{}{}", &video_folder, &videostr, &format_string);
   let _ = fs::create_dir_all("videos");
   let path = std::path::Path::new(&video_title);
   video.download(path).await.unwrap();
 }
 
+#[tauri::command]
+fn open() {
+  let _ = fs::create_dir_all("videos");
+  let path = "videos";
+
+  if let Err(e) = opener::open(path) {
+    eprintln!("{:?}", e);
+  }
+}
+
 fn main() {
   tauri::Builder::default()
-  .invoke_handler(tauri::generate_handler![download])
+  .invoke_handler(tauri::generate_handler![download, open])
   .run(tauri::generate_context!())
   .expect("error while running tauri application");
 }
--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,11 @@ function App() {
     setDMessage("Succesful Download")
   }
 
+  function openFolder() {
+    console.log("a")
+    invoke("open")
+  }
+
   function getFormat() {
     setFormat(prev => !prev)
   }
@@ -49,13 +54,15 @@ function App() {
         <input className=" border-b-2 w-[400px] border-white outline-none focus:border-b-red-600 duration-300 focus:ease-out  " onChange={(e) => setInput(e.currentTarget.value)} placeholder="Video URL"/>
         <button className=" font-bold border-2 border-red-600 w-28 h-12 mx-5 rounded-xl text-red-500 hover:bg-red-600 hover:text-white hover:drop-shadow-md hover:shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f] hover:shadow-red-600 ease-in-out duration-500" type="submit">Download</button>
       </form>
-
       <div className={`flex justify-center items-center`}>
           <svg aria-hidden="true" className={`w-12 h-12 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600 ${downloadingState ? "opacity-100" : "opacity-0"}`} viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
             <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
           </svg>
           <p className="mx-4 font-bold text-xl">{dMessage}</p>
+      </div>
+      <div className="flex items-center justify-center h-32">
+        <button className="rounded-xl border-2 w-48 top-0 right-0 m-[10%] hover:white hover:text-white hover:drop-shadow-md hover:shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f] hover:shadow-white transition-all ease-in-out duration-500" onClick={() => openFolder()}>Downloaded Videos</button>
       </div>
     </main>
   )
